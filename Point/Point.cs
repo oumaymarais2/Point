5c4cf2ff6bbc044d095551670cab9ab670e4ca7a
DELL <DELL@DESKTOP-64KR7SG>
2022-11-10 20:43:00 +0100

    Add project files.

---
+++ Point/Point.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    internal class Point
+    {
+        //L'attribut Abscisse
+        private double Abscisse;            
+        //L'attribut Ordonne
+        private double Ordonne;             
+        //Le getter d' Abscisse
+        public double getAbscisse()        
+        {
+            return Abscisse;
+        }
+        //Le setter d' Abscisse
+        public void setAbscisse(double A)  
+        {
+            Abscisse = A;
+        }
+
+        //Le getter de  Ordonne
+        public double getOrdonne()          
+        {
+            return Ordonne;
+        }
+        //Le setter de  Ordonne
+        public void setOrdonne(double B)    
+        {
+            Ordonne = B;
+        }
+        //Le constructeur par défaut
+        public Point()                    
+        {
+        }
+        //Le constructeur d'initialisation
+        public Point(double A, double B)  
+        {
+            Abscisse = A;
+            Ordonne = B;
+        }
+        //La méthode Distance
+        public double Distance()              
+        {
+            return Math.Sqrt(Abscisse * Abscisse + Ordonne * Ordonne);
+        }
+        
+    }
+}
---
+++ Point/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, n;
+            //instanciation d'un objet Point
+            Point p = new Point();                   
+            Console.Out.Write("Donner l'abscisse: ");
+            A = double.Parse(Console.In.ReadLine());
+            Console.Out.Write("Donner l'ordonne: ");
+            B = double.Parse(Console.In.ReadLine());
+            //l'appel des méthodes sur l'objet p
+            p.setAbscisse(A);
+            p.setOrdonne(B);
+            n = p.Distance();
+            Console.Out.WriteLine("La distance du point (" + p.getAbscisse() + "," + p.getOrdonne() + ") est: " + n);
+            Console.ReadKey();
+            
+        }
+    }
+}
